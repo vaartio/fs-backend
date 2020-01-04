bd476174183051dc1ed6b64d56da230dfa31cb1c
Lari Vaartio <lari@layer.fi>
2020-01-04 21:05:25 +0200

    uibuild

---
+++ build/precache-manifest.03e5e429b33d379c4c726fb4f5ce81b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7433d9876f1ab73e4690c08c8d12ac33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e96f6ecdefc45c2650cd",
+    "url": "/static/js/2.2558dea3.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.2558dea3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "684d4d51ba2bd2eb1c59",
+    "url": "/static/js/main.7b5bf88b.chunk.js"
+  },
+  {
+    "revision": "ec2ac39fbc5044a4cece",
+    "url": "/static/js/runtime-main.97c5a5d6.js"
+  }
+]);
