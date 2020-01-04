cc52fa0de183f39f51938dd228e788cd0487baea
Lari Vaartio <lari@layer.fi>
2020-01-04 21:08:31 +0200

    uibuild

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('build'))
+
 let persons = [
   {
     "name": "Arto Hellas",
